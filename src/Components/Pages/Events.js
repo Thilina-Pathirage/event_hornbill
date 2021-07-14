161d99c998e226d6599346600e02ab65acb35c75
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-07-14 15:13:42 +0530

    new hover effect

--- src/Components/Pages/Events.js
+++ src/Components/Pages/Events.js
@@ -3,9 +3,31 @@ import React, { useState, useEffect } from 'react';
 import { MDBCol, MDBRow, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdb-react-ui-kit';
 import './Events.css';
 import { Link } from 'react-router-dom';
-import CoverImage from '../images/eventcover.png'; // Tell Webpack this JS file uses this image
+import CoverImage from '../images/eventcover.png';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Events = () => {
+    
+    const [bgColor, setBgColor] = useState("0, 0, 0,");
+    const [opacity, setOpacity] = useState(1);
+
+    const useStyles = makeStyles (() => ({
+
+        "@global": {
+          body: {
+            backgroundImage: `linear-gradient(to left, #000,#000, #000, rgb(0, 0, 0, 0) )`,
+            backgroundColor: `rgb( ${bgColor} ${opacity})`,
+            transitionDuration: "0.5s",
+          },
+        },
+    }))
+
+    const classes = useStyles();
+
+    const setStyles = (Opc,bg) => {
+         setBgColor(bg);
+        setOpacity(Opc)
+    }
 
     const [events, setEvents] = useState([]);
     useEffect(() => {
@@ -32,7 +54,7 @@ const Events = () => {
         return (
 
             <div key={k}>
-                <div className="event-card">
+                <div className="event-card" id="event-card" >
                     <MDBRow>
                         <MDBCol className="col-one center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6" md="3">
                             <div className="event-cover img-fluid" >
@@ -49,9 +71,10 @@ const Events = () => {
                                 <MDBCardText>
                                     {i.description}
                                 </MDBCardText>
-                                <Link to="" className="add-button" onClick={() => addEvent(i.title)}>Add to Calendar</Link>
-                                {i.online_link ? <a className="my-button2" href={i.online_link}>Join now!</a> : ""}
-                                <Link to="" className="my-button2" onClick={() => deleteEvent(i._id)}>Delete</Link>
+                                <Link onMouseEnter={() => {setStyles(1, "0, 59, 187,")}} onMouseLeave={() => {setStyles(1,"0, 0, 0,")}} to="" className="add-button" onClick={() => addEvent(i.title)}>Add to Calendar</Link>
+                                {i.online_link ? <a onMouseEnter={() => {setStyles(1, "6, 187, 0,")}} onMouseLeave={() => {setStyles(1,"0, 0, 0,")}}  className="my-button2" href={i.online_link}>Join now!</a> : ""}
+                                <Link onMouseEnter={() => {setStyles(1, "255, 123, 0,")}} onMouseLeave={() => {setStyles(1,"0, 0, 0,")}}  to="" className="my-button2" onClick={() => deleteEvent(i._id)}>Delete</Link>
+                            
                             </MDBCardBody>
                         </MDBCol>
                     </MDBRow>
@@ -59,8 +82,9 @@ const Events = () => {
             </div>
         );
     })
+
     return (
-        <div>
+        <div className={classes.testDiv} >
             <br />
             {eventItem}
         </div>
